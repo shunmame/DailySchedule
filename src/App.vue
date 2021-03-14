c38e4e3e7bf4996b7ab15427e4477972f7db498d
shunmame <ac6328mats@g.kumamoto-nct.ac.jp>
2021-03-14 20:55:59 +0900

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <chart></chart>
+  </div>
+</template>
+
+<script>
+import chart from "./components/DailySchedule";
+
+export default {
+  components: {
+    chart,
+  },
+};
+</script>
+
+<style>
+</style>
