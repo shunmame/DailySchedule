cb9b610c32295a07596a23dca272f270f57f4cb6
shunmame <ac6328mats@g.kumamoto-nct.ac.jp>
2021-03-19 21:29:43 +0900

    install chartjs-plugin-datalabels

--- src/components/DailySchedule.vue
+++ src/components/DailySchedule.vue
@@ -1,5 +1,7 @@
 <script>
 import { Pie } from "vue-chartjs";
+// import "chartjs-plugin-datalabels";
+import ChartDataLabels from "chartjs-plugin-datalabels";
 
 export default {
   extends: Pie,
@@ -10,34 +12,26 @@ export default {
         labels: ["January", "February", "March", "April", "May", "June"],
         datasets: [
           {
-            label: "Bar Dataset",
+            // label: "Bar Dataset",
             data: [10, 20, 30, 40, 50, 30],
-            // backgroundColor: [
-            //   "rgba(255, 99, 132, 0.2)",
-            //   "rgba(54, 162, 235, 0.2)",
-            //   "rgba(255, 206, 86, 0.2)",
-            //   "rgba(75, 192, 192, 0.2)",
-            //   "rgba(153, 102, 255, 0.2)",
-            //   "rgba(255, 159, 64, 0.2)",
-            // ],
-            // borderColor: [
-            //   "rgba(255, 99, 132, 1)",
-            //   "rgba(54, 162, 235, 1)",
-            //   "rgba(255, 206, 86, 1)",
-            //   "rgba(75, 192, 192, 1)",
-            //   "rgba(153, 102, 255, 1)",
-            //   "rgba(255, 159, 64, 1)",
-            // ],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
             borderWidth: 1,
           },
-          //   {
-          //     label: "Line Dataset",
-          //     data: [10, 50, 20, 30, 30, 40],
-          //     borderColor: "#CFD8DC",
-          //     fill: false,
-          //     type: "line",
-          //     lineTension: 0.3,
-          //   },
         ],
       },
       options: {
@@ -46,10 +40,33 @@ export default {
           display: true,
           text: "1日のスケジュール",
         },
+        plugins: {
+          labels: [
+            {
+              render: "label",
+            },
+            {
+              render: "label",
+            },
+            {
+              render: "label",
+            },
+            {
+              render: "label",
+            },
+            {
+              render: "label",
+            },
+            {
+              render: "label",
+            },
+          ],
+        },
       },
     };
   },
   mounted() {
+    this.addPlugin(ChartDataLabels);
     this.renderChart(this.data, this.options);
   },
 };
